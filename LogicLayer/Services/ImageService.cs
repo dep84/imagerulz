baef02742ef2f4bb64209617cfdebfd5b708c76b
Kasper Purunen <kasper.purunen@outlook.com>
2018-06-20 13:54:27 +0200

    adds imageservice for saving images to disc

---
+++ LogicLayer/Services/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace DomainLayer.Services
+{
+    public class ImageService
+    {
+        public void SaveImageToFile(string path, byte[] imageData)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(path)) throw new ArgumentNullException($"{nameof(path)} most not be empty.");
+
+                if (imageData == null) return;
+
+                File.WriteAllBytes($"{path}/{Guid.NewGuid()}", imageData);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+    }
+}
